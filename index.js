07587b86ef161e92eea7370ac497bf968790d80c
Kingsley <arafatrejoice@gmail.com>
2022-05-28 17:17:33 +0100

    fix: Fix error error

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const simpleGit = require('simple-git');
 
 
 const DATE = moment().subtract(11, 'months').format();
-simpleGit().add("./*").commit("fix: Fix null error", {'--date': DATE}).push();
+simpleGit().add("./*").commit("fix: Fix error error", {'--date': DATE}).push();
